cce94532ce6b81ad04f0750bffe2a0083a485ea6
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2023-09-29 18:38:41 +0300

    Fixed arrival time in flight factory to be after departure time

--- database/factories/FlightFactory.php
+++ database/factories/FlightFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use DateInterval;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,12 +17,21 @@ class FlightFactory extends Factory
      */
     public function definition(): array
     {
+        $departureTime = $this->faker->dateTimeBetween('-1 year', '+1 year');
+        
+         // Generate a random number of hours between 1 and 24 (adjust the range as needed)
+         $randomHours = $this->faker->numberBetween(1, 19);
+
+         // Create an end date by adding the random number of hours to the departure time
+         $endDate = clone $departureTime;
+         $endDate->add(new DateInterval("PT{$randomHours}H"));
+
         return [
             'flight_number' => fake()->randomNumber(4, true),
             'departure_city' => fake()->city(),
             'arrival_city' => fake()->city(),
-            'departure_time' => fake()->dateTimeBetween('-1 year', '+1 year')->format('Y-m-d H:i:s'),
-            'arrival_time' => fake()->dateTimeBetween('-1 year', '+1 year')->format('Y-m-d H:i:s'),
+            'departure_time' => $departureTime->format('Y-m-d H:i:s'),
+            'arrival_time' => $endDate->format('Y-m-d H:i:s'),
         ];
     }
 }
